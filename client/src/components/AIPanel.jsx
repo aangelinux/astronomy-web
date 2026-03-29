beeb8be577d1003b411a4b3bfb9aad0c5b814a35
Angelina Larsson <al227bn@student.lnu.se>
2026-03-29 16:12:01 +0200

    Add styles to info panels

--- client/src/components/AIPanel.jsx
+++ client/src/components/AIPanel.jsx
@@ -13,22 +13,29 @@ function AIPanel() {
 	const [description, setDescription] = useState("")
 
 	useEffect(() => {
-		const fetchDescription = async () => {
-			const input = await getNeo(neo)
-			const text = await fetchGenAI(input)
-			setDescription(text)
-		}
-		fetchDescription()
+		// const fetchDescription = async () => {
+		// 	const input = await getNeo(neo)
+		// 	const text = await fetchGenAI(input)
+		// 	setDescription(text)
+		// }
+		// fetchDescription()
 	}, [neo])
 
 	const card = (
 		<CardContent>
-			{description}
+			AI generated description here ...
 		</CardContent>
 	)
 
 	return (
-		<Box sx={{ minWidth: 275 }}>
+		<Box sx={{ 
+			minWidth: 275,
+			boxShadow: 1,
+			borderRadius: 2,
+			fontFamily: 'GoogleSans',
+			fontWeight: 'bold',
+			textAlign: 'center'
+		}}>
 			<Card variant="outlined">{card}</Card>
 		</Box>
 	)
--- client/src/components/AttributePanel.jsx
+++ client/src/components/AttributePanel.jsx
@@ -2,7 +2,7 @@
  * Panel displaying NEO attributes.
  */
 
-import { Box, Card, CardContent } from '@mui/material'
+import { Box, Card, CardContent, List, ListItem, Typography } from '@mui/material'
 import { useAppContext } from '../hooks/context.jsx'
 import { useState, useEffect } from 'react'
 import { getNeo } from '../api/neos.js'
@@ -12,27 +12,60 @@ function AttributePanel() {
 	const [attributes, setAttributes] = useState([])
 
 	useEffect(() => {
-		const fetchNeo = async () => {
+		async function fetchNeo() {
 			const data = await getNeo(neo)
-			const vales = Object.values(data)
-			setAttributes(vales)
+			setAttributes(data)
 		}
 		fetchNeo()
 	}, [neo])
 
+	const mapAttributes = () => {
+		const map = [
+			{ 'SPKID': attributes['spkid'] },
+			{ 'Name': attributes['name'] },
+			{ 'Earth MOID': attributes['earth_moid_ld'] },
+			{ 'Magnitude': attributes['magnitude'] },
+			{ 'Rotation (hours)': attributes['rotation_hours'] },
+			{ 'PHA': attributes['pot_hazardous_asteroid'] }
+		]
+		const filteredMap = map.filter((entry) => 
+			(Object.values(entry)[0] !== null || undefined))
+
+		return filteredMap
+	}
+
 	const card = (
 		<CardContent>
-			<ul>
-				{attributes.map((attribute, index) => (
-					<li key={index}>{attribute}</li>
+			<Typography variant='h6' sx={{
+				fontFamily: 'GoogleSans',
+				fontWeight: 'bold',
+				paddingTop: 1,
+				border: 'none'
+			}}>
+				Attributes
+			</Typography>
+			<List>
+				{mapAttributes().map((attribute, index) => (
+					<ListItem key={index}>
+						{`${Object.keys(attribute)}: ${Object.values(attribute)}`}
+					</ListItem>
 				))}
-			</ul>
+			</List>
 		</CardContent>
 	)
 
 	return (
-    <Box sx={{ minWidth: 275 }}>
-      <Card variant="outlined">{card}</Card>
+		<Box sx={{ 
+			minWidth: 225,
+			boxShadow: 1,
+			fontFamily: 'GoogleSans',
+			fontSize: '.95rem',
+			textAlign: 'center',
+			letterSpacing: 1,
+			lineHeight: .8,
+			listStyleType: 'none',
+		}}>      
+		<Card variant="outlined">{card}</Card>
     </Box>
 	)
 }

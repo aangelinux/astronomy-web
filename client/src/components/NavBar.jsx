5bfb07d70b5cd8a9b3465d4c75228fa570b9138f
Angelina Larsson <al227bn@student.lnu.se>
2026-03-30 13:52:45 +0200

    Add styles to navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,15 +2,31 @@
  * Navigation bar for authorized users.
  */
 
-import { Link } from 'react-router'
+import { AppBar, Toolbar, Link, Avatar } from '@mui/material'
 
 function NavBar() {
+	const linkStyle = {
+		color: 'white',
+		fontFamily: 'GoogleSans',
+		fontWeight: 'bold',
+		fontSize: '1.1rem',
+		textAlign: 'center',
+		margin: 1.7
+	}
+
 	return (
-		<nav>
-			<Link to="/">Auth</Link> | {' '}
-			<Link to="/dashboard">Dashboard</Link> | {' '}
-			<Link to="/neos">Neos</Link> | {' '}
-		</nav>
+		<AppBar sx={{ maxHeight: 50 }}>
+			<Toolbar sx={{ backgroundColor: '#0b0f1a', boxShadow: 10 }}>
+				<Avatar 
+					src="../../assets/astronomy.png" 
+					alt="Astronomy Icon"
+					sx={{ margin: 2.5 }}>
+				</Avatar>
+				<Link href="/" sx={linkStyle}>Auth</Link>
+				<Link href="/dashboard" sx={linkStyle}>Dashboard</Link>
+				<Link href="/neos" sx={linkStyle}>NEOs</Link>
+			</Toolbar>
+		</AppBar>
 	)
 }
 
